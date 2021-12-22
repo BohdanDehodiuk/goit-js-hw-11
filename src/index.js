afe30f73555aa5e37896605bc4971356bf12cce8
Dehodiuk Bohdan <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-12-22 20:04:09 +0200

    finish hw#11

--- src/index.js
+++ src/index.js
@@ -2,3 +2,79 @@ import './sass/main.scss';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import PictureApiService from './js/fetchImages';
+import LoadMoreBtn from './js/loadBtn';
+import picTamplate from './templates/cards';
+
+const Refs = {
+  form: document.getElementById('search-form'),
+  picturesContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const lightbox = new SimpleLightbox('.gallery a');
+const pictureApiService = new PictureApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+Refs.form.addEventListener('submit', imageSerch);
+loadMoreBtn.refs.button.addEventListener('click', loadMorePic);
+
+function loadMorePic() {
+  pictureApiService.fetchPictures().then(data => {
+    if (data.totalHits < pictureApiService.page * 40) {
+      loadMoreBtn.hide();
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+    createPicCard(data);
+    lightbox.refresh();
+  });
+}
+
+function imageSerch(e) {
+  e.preventDefault();
+  pictureApiService.value = e.currentTarget.elements.searchQuery.value.trim();
+  pictureApiService.clearPageCount();
+  pictureApiService
+    .fetchPictures()
+    .then(data => {
+      if (pictureApiService.value === '' || data.hits.length === 0) {
+        Refs.form.reset();
+        return Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      } else if (data.totalHits < 40) {
+        loadMoreBtn.hide();
+        Notiflix.Notify.success(`Hooray! We found  ${data.totalHits}  images.`);
+        return createPicCard(data);
+      }
+      clearPicContainer();
+      loadMoreBtn.show();
+      createPicCard(data);
+      Notiflix.Notify.success(`Hooray! We found  ${data.totalHits}  images.`);
+    })
+    .then(e => {
+      lightbox.refresh();
+    })
+    .catch(e => {
+      Notiflix.Notify.info(`Something went wrong please try again`);
+      console.log(e);
+    });
+}
+
+function createPicCard(data) {
+  return data.hits.map(
+    ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return Refs.picturesContainer.insertAdjacentHTML(
+        'beforeend',
+        picTamplate({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }),
+      );
+    },
+  );
+}
+
+function clearPicContainer() {
+  Refs.picturesContainer.innerHTML = '';
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,31 +1,30 @@
 const axios = require('axios');
 
 export default class DataFetch {
-    constructor() {
-        this.findInput = "";
-        this.page = 1;
+  constructor() {
+    this.searchValue = '';
+    this.page = 1;
+  }
+  async fetchPictures() {
+    const URL = `https://pixabay.com/api/?key=24901331-8d9807a1a9b1c2247500253bc&q=${this.searchValue}&page=${this.page}&per_page=40&image_type=photo,orientation=horizontal,safesearch=true,`;
+    try {
+      const response = await axios.get(URL);
+      this.PageCounter();
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
-
-    async fetchImages () {
-        const URL = `https://pixabay.com/api/?key=24901331-8d9807a1a9b1c2247500253bc&q=${this.findInput}&page=${this.page}&per_page=40&image_type=photo,orientation=horizontal,safesearch=true,`;
-        try {
-            const response = await axios.get(URL);
-            this.pageCounter()
-            return response.data;
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    get valeu() {
-        return this.findInput;
-    }
-    set valeu(valeu) {
-        this.findInput = value;
-    }
-    clearPageCount() {
-        this.page = 1;
-    }
-    pageCounter() {
-        this.page += 1;
-    }
+  }
+  get value() {
+    return this.searchValue;
+  }
+  set value(value) {
+    this.searchValue = value;
+  }
+  clearPageCount() {
+    this.page = 1;
+  }
+  PageCounter() {
+    this.page += 1;
+  }
 }
